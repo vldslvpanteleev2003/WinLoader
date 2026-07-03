54047f624ece03d5a865194e9261398a009d2957
Vladislav Panteleev <vldslvpanteleev2003@gmail.com>
2026-07-04 00:26:05 +0300

    Update Loader.cpp

--- Loader.cpp
+++ Loader.cpp
@@ -18,17 +18,17 @@ int main()
     int length = file.tellg();
     file.seekg(0, file.beg);
 
-    std::vector<uint8_t> Buffer(length);
-    file.read(reinterpret_cast<char*>(Buffer.data()), length);
+    std::vector<uint8_t> buffer(length);
+    file.read(reinterpret_cast<char*>(buffer.data()), length);
 
-    IMAGE_DOS_HEADER* dos{ reinterpret_cast<IMAGE_DOS_HEADER*>(Buffer.data()) };
+    IMAGE_DOS_HEADER* dos{ reinterpret_cast<IMAGE_DOS_HEADER*>(buffer.data()) };
 
     if (dos->e_magic != IMAGE_DOS_SIGNATURE)
     {
         std::cout << std::format("File is not pe") << std::endl;
     }
 
-    IMAGE_NT_HEADERS* nt{ reinterpret_cast<IMAGE_NT_HEADERS*>(Buffer.data() + dos->e_lfanew) };
+    IMAGE_NT_HEADERS* nt{ reinterpret_cast<IMAGE_NT_HEADERS*>(buffer.data() + dos->e_lfanew) };
 
     IMAGE_FILE_HEADER* fileHeader{ &nt->FileHeader };
 
@@ -40,58 +40,51 @@ int main()
 
     WORD numberOfSec{ fileHeader->NumberOfSections };
 
-    BYTE* ImageBase{ reinterpret_cast<BYTE*>(VirtualAlloc(nullptr, imageSize, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE)) };
+    BYTE* imageBase{ reinterpret_cast<BYTE*>(VirtualAlloc(nullptr, imageSize, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE)) };
 
-    IMAGE_SECTION_HEADER* section{ IMAGE_FIRST_SECTION(nt) };
+    IMAGE_SECTION_HEADER* sectionHeader{ IMAGE_FIRST_SECTION(nt) }; /*Section Headers*/
 
-    memcpy(ImageBase, Buffer.data(), headerSize);
+    memcpy(imageBase, buffer.data(), headerSize); /*копируем заголовок файла*/
 
-    for (int i = 0; i < numberOfSec; i++)
+    for (int i = 0; i < numberOfSec; i++) /*копируем секции*/
     {
-        void* dst{ ImageBase + section[i].VirtualAddress };
+        void* dst{ imageBase + sectionHeader[i].VirtualAddress };
 
-        const void* src{ Buffer.data() + section[i].PointerToRawData };
+        const void* src{ buffer.data() + sectionHeader[i].PointerToRawData };
 
-        memcpy(dst, src, section[i].SizeOfRawData);
+        memcpy(dst, src, sectionHeader[i].SizeOfRawData);
     }
 
     IMAGE_DATA_DIRECTORY importDir{ optionalHeader->DataDirectory[1] };
 
     DWORD importDirRva{ importDir.VirtualAddress };
 
-    IMAGE_IMPORT_DESCRIPTOR* importDescriptor{ reinterpret_cast<IMAGE_IMPORT_DESCRIPTOR*>(ImageBase + importDirRva) };
+    IMAGE_IMPORT_DESCRIPTOR* importDescriptor{ reinterpret_cast<IMAGE_IMPORT_DESCRIPTOR*>(imageBase + importDirRva) };
 
     /*IAT резолвинг*/
     for (; importDescriptor->Name != 0; importDescriptor++)
     {
-        const char* dllname{ reinterpret_cast<const char*>(ImageBase + importDescriptor->Name) };
-        std::cout << dllname << std::endl;
-        DWORD OriginalFirstThunkRva{ importDescriptor->OriginalFirstThunk };
-        DWORD FirstThunkRva{ importDescriptor->FirstThunk };
-        uint64_t* IATFunctionAddress{ reinterpret_cast<uint64_t*>(ImageBase + FirstThunkRva) };
-        std::cout << std::format("OriginalFirstThunk: {:x}\n\n\n", OriginalFirstThunkRva);
-        HMODULE module{ LoadLibraryA(dllname) };
-        for (uint64_t* rvaFunction{ reinterpret_cast<uint64_t*>(ImageBase + OriginalFirstThunkRva) }; *rvaFunction != 0; rvaFunction = rvaFunction + 1, IATFunctionAddress = IATFunctionAddress + 1)
+        const char* dllName{ reinterpret_cast<const char*>(imageBase + importDescriptor->Name) };
+        DWORD originalFirstThunkRva{ importDescriptor->OriginalFirstThunk };
+        DWORD firstThunkRva{ importDescriptor->FirstThunk };
+        uintptr_t* IATFunctionAddress{ reinterpret_cast<uintptr_t*>(imageBase + firstThunkRva) };
+        HMODULE module{ LoadLibraryA(dllName) };
+        for (uintptr_t* rvaFunction{ reinterpret_cast<uintptr_t*>(imageBase + originalFirstThunkRva) }; *rvaFunction != 0; rvaFunction = rvaFunction + 1, IATFunctionAddress = IATFunctionAddress + 1)
         {
             /*Поиск адресов по FunctionName*/
             if (!(*rvaFunction & 0x8000000000000000)) /*Проверка на флаг ordinal*/
             {
-                std::cout << std::format("rvaFunction: {:x}\n\n", *rvaFunction);
-                NameFunction* functionName{ reinterpret_cast<NameFunction*>(ImageBase + *rvaFunction) };
-                std::cout << std::format("FunctionName: {}\n\n", functionName->structFunctionName);
+                NameFunction* functionName{ reinterpret_cast<NameFunction*>(imageBase + *rvaFunction) };
                 FARPROC proc{ GetProcAddress(module, functionName->structFunctionName) };
-                uint64_t functionAddress{ reinterpret_cast<uint64_t>(proc) };
-                std::cout << std::format("FunctionAddress: {:x}\n\n\n", functionAddress);
+                uintptr_t functionAddress{ reinterpret_cast<uintptr_t>(proc) };
                 *IATFunctionAddress = functionAddress;
             }
             /*Поиск адресов ordinal*/
             else 
             {   
                 WORD ordinal{ static_cast<WORD>(*rvaFunction) };
-                std::cout << std::format("Ordinal: {}\n\n", ordinal);
                 FARPROC proc{ GetProcAddress(module, reinterpret_cast<LPCSTR>(ordinal)) };
-                uint64_t functionAddress{ reinterpret_cast<uint64_t>(proc) };
-                std::cout << std::format("FunctionAddress: {:x}\n\n\n", functionAddress);
+                uintptr_t functionAddress{ reinterpret_cast<uintptr_t>(proc) };
                 *IATFunctionAddress = functionAddress;
             }
         }
@@ -101,52 +94,44 @@ int main()
 
     /*relocations резолвинг*/
     IMAGE_DATA_DIRECTORY relocDir{ optionalHeader->DataDirectory[5] };
-    if (DWORD relocDirRva{ relocDir.VirtualAddress })
+    if (DWORD relocDirRva{ relocDir.VirtualAddress }) /*провека на наличие reloc секции*/
     {
-        uint64_t blockSize = 0;
+        uintptr_t blockSize = 0;
         do
         {
-            StructRelocSection* relocSection = reinterpret_cast<StructRelocSection*>(ImageBase + relocDir.VirtualAddress + blockSize);
-            DWORD count = (relocSection->SizeOfBlock - sizeof(StructRelocSection::VirtualAddress) - sizeof(StructRelocSection::SizeOfBlock)) / sizeof(WORD);
-            if (!relocSection->SizeOfBlock)
+            StructRelocSection* relocSection = reinterpret_cast<StructRelocSection*>(imageBase + relocDir.VirtualAddress + blockSize);
+            DWORD count = (relocSection->SizeOfBlock - sizeof(StructRelocSection::VirtualAddress) - sizeof(StructRelocSection::SizeOfBlock)) / sizeof(WORD); /*считаем количество записей в одном блоке, которые указывают на места с релокациями*/
+            if (!relocSection->SizeOfBlock) /*проверка sizeOfblock параметра у блока*/
             {
                 break;
             }
-            std::cout << std::format("{}\n", count);
-            uint64_t delta = reinterpret_cast<uint64_t>(ImageBase) - optionalHeader->ImageBase;
-            std::cout << std::format("Delta: {:x}\nImageBase: {:x}\nActualImageBase: {:x}\n\n", delta, optionalHeader->ImageBase, reinterpret_cast<uint64_t>(ImageBase));
+            uintptr_t delta = reinterpret_cast<uintptr_t>(imageBase) - optionalHeader->ImageBase;
             for (int i{ 0 }; i < count; i++)
             {
-                if (relocSection->TypeOffset[i])
+                if (relocSection->TypeOffset[i]) /*расчитываем адрес значения требующее релокации и меняем значение*/
                 {
                     WORD entry{ relocSection->TypeOffset[i] };
                     WORD type = entry >> 12;
                     WORD offset = entry & 0x0FFF;
-                    std::cout << std::format("Type: {:x}\n", type);
-                    std::cout << std::format("Ofsset: {:x}\n", offset);
-                    uintptr_t* relocAddress = reinterpret_cast<uint64_t*>(ImageBase + relocSection->VirtualAddress + offset);
-                    std::cout << std::format("Reloc address: {:x}\n", reinterpret_cast<uintptr_t>(relocAddress));
-                    std::cout << std::format("Reloc before: {:x}\n", *relocAddress);
+                    uintptr_t* relocAddress = reinterpret_cast<uint64_t*>(imageBase + relocSection->VirtualAddress + offset);
                     *relocAddress = *relocAddress + delta;
-                    std::cout << std::format("Reloc after: {:x}\n", *relocAddress);
                 }
             }
             blockSize += relocSection->SizeOfBlock;
 
         } while (true);
     }
-    else
-    {
-        std::cout << std::format("Section .reloc wasn't found.\n");
-    }
     /*relocations резолвинг*/
 
     /*Вызов DLLmain*/
+
+    std::cout << std::format("Calling dllMain\n");
+
     using DllMain_t = BOOL(WINAPI*)(HINSTANCE, DWORD, LPVOID);
 
-    DllMain_t dllMain = reinterpret_cast<DllMain_t>(ImageBase + optionalHeader->AddressOfEntryPoint);
+    DllMain_t dllMain = reinterpret_cast<DllMain_t>(imageBase + optionalHeader->AddressOfEntryPoint);
 
-    dllMain((HINSTANCE)ImageBase, DLL_PROCESS_ATTACH, nullptr);
+    dllMain((HINSTANCE)imageBase, DLL_PROCESS_ATTACH, nullptr);
 
     /*Вызов DLLmain*/
 }
